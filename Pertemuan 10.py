14b059b1fbb759834f9e06b3728cb022262efd17
ERMAYANI <yaniyaniirmayani@gmail.com>
2024-12-30 09:56:23 +0800

    Add files via upload

---
+++ Pertemuan 10.py
@@ -0,0 +1,40 @@
+print ("perulangan 1")
+for i in range (5):
+    print (i)
+    
+print ("perulangan 2")
+for  i in range (0, 11, 2):
+    print(i)
+    
+print ("perulangan 3")
+for i in range (1, 6):
+    print (i)
+
+print ("perulanagan dengan step")
+for i in range (1,20,3):
+    print (i)
+
+print ("perulangan total number")
+numbers = [10, 20, 30 ,40, 50]
+total = 0
+for num in numbers:
+    total += num
+print("Total nilai:",total)
+
+#Membuat Perulangan For dengan List
+print ("perulangan 1")
+for i in  [1, 2, 3, 4, 5]:
+    print("ini pengulangan ke -",i)
+
+# Membuat Perulangan For dengnan List dengan isi String
+print ("perulangan sebanyak 8 kali")
+for i in ["Rawon", "Nasi Kuning", "Soto Madura", "Kupat Tahu ", "Kerak Telor", "Rendang Batoko", "Pempek Selam", "Ayam Betetu"] :
+    print (i, "adalah masakan khas nusantara...")
+
+#Membuat perulangan for dengan string
+
+for i in ["a","b","c","d","e"] :
+    print (i, "ini adalah alfabet" )
+
+
+    
